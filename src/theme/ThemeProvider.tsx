4f25d2604afbb0f1118e7ca5228e081b329f4bd8
halilibrahimcelik <halil.celik139@gmail.com>
2024-06-26 20:58:36 +0300

    feat:removed unused code.

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,17 +1,8 @@
 import { createContext, useContext, useMemo, useState } from 'react';
-import {
-  MD2DarkTheme,
-  MD2LightTheme,
-  MD2Theme,
-  MD3DarkTheme,
-  MD3LightTheme,
-  MD3Theme,
-  useTheme,
-} from 'react-native-paper';
+import { MD2Theme, MD3Theme, useTheme } from 'react-native-paper';
 interface InitialState {
   isDarkMode: boolean;
   toggleTheme: () => void;
-  theme: MD2Theme | MD3Theme;
 }
 
 export const ThemeContext = createContext<InitialState | null>(null);
@@ -25,22 +16,13 @@ export const useExampleTheme = () => useTheme<MD2Theme | MD3Theme>();
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
-  const [themeVersion, setThemeVersion] = useState<2 | 3>(3);
 
-  const theme = useMemo(() => {
-    if (themeVersion === 2) {
-      return isDarkMode ? MD2DarkTheme : MD2LightTheme;
-    }
-
-    return isDarkMode ? MD3DarkTheme : MD3LightTheme;
-  }, [isDarkMode, themeVersion]);
   const value = useMemo(
     () => ({
       isDarkMode,
       toggleTheme: () => setIsDarkMode((oldValue) => !oldValue),
-      theme,
     }),
-    [theme]
+    [isDarkMode]
   );
   return (
     <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
