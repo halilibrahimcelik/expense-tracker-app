8de99e0ba9263d7b7b6f7a4d79441c93075c28ed
halilibrahimcelik <halil.celik139@gmail.com>
2024-07-12 11:24:13 +0300

    feat:sigin authentication connected to db.

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,8 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { Button, TextInput, useTheme } from 'react-native-paper';
 import CustomErrorMessage from '../UI/CustomErrorMessage';
-import { ErrorState } from '@/types';
+import { ErrorState, STACK_NAMES, StackNavigation } from '@/types';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '@/firebase/firebase.config';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { nanoid } from 'nanoid';
+import { useNavigation } from '@react-navigation/native';
 
 type Props = {};
 type UserData = {
@@ -15,7 +20,9 @@ type ErrorType = {
 };
 const SignInForm = (props: Props) => {
   const theme = useTheme();
+  const navigation = useNavigation<StackNavigation>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
   const [userData, setUserData] = useState<UserData>({
     email: '',
     password: '',
@@ -32,22 +39,66 @@ const SignInForm = (props: Props) => {
       errorMessage: '',
     },
   });
-  const onSubmit = () => {
+
+  useEffect(() => {
+    const validatePassword = (text: string) => {
+      if (text.trim() === '') {
+        setErrorState((prev) => ({
+          ...prev,
+          password: { isError: true, errorMessage: 'Password is required' },
+        }));
+      } else if (text.length < 6 && text.trim() !== '') {
+        setErrorState((prev) => ({
+          ...prev,
+          password: {
+            isError: true,
+            errorMessage: 'Password must be at least 6 characters',
+          },
+        }));
+      } else {
+        setErrorState((prev) => ({
+          ...prev,
+          password: { isError: false, errorMessage: '' },
+        }));
+      }
+    };
+
+    const validateEmail = (text: string) => {
+      if (!text.trim()) {
+        setErrorState((prev) => ({
+          ...prev,
+          email: { isError: true, errorMessage: 'Email is required' },
+        }));
+      } else {
+        setErrorState((prev) => ({
+          ...prev,
+          email: { isError: false, errorMessage: '' },
+        }));
+      }
+    };
+
+    if (isSubmitted) {
+      validatePassword(password);
+    }
+    if (isSubmitted && userData.email) {
+      validateEmail(userData.email);
+    }
+  }, [isSubmitted, password, userData.email]);
+  const onSubmit = async () => {
+    setIsSubmitted(true);
     if (userData.email === '') {
       setErrorState((prev) => ({
         ...prev,
         email: { isError: true, errorMessage: 'Email is required' },
       }));
-      return;
     }
     if (userData.password === '') {
       setErrorState((prev) => ({
         ...prev,
         password: { isError: true, errorMessage: 'Password is required' },
       }));
-      return;
     }
-    if (userData.password.length < 6) {
+    if (userData.password !== '' && userData.password.length < 6) {
       setErrorState((prev) => ({
         ...prev,
         password: {
@@ -55,14 +106,34 @@ const SignInForm = (props: Props) => {
           errorMessage: 'Password must be at least 6 characters',
         },
       }));
-      return;
     }
-    if (userData.email && userData.password) {
+
+    if (userData.email && password) {
       setIsLoading(true);
+      const data = {
+        email: userData.email,
+        password,
+      };
+      setUserData(data);
+      const res = await signInWithEmailAndPassword(
+        auth,
+        userData.email,
+        password
+      );
+      if (res.user.uid) {
+        setIsLoading(false);
+        setIsSubmitted(false);
+        await AsyncStorage.setItem('userId', res.user.uid);
+        await AsyncStorage.setItem('isLoggedIn', 'true');
+        navigation.navigate(STACK_NAMES.ExpenseForm, {
+          slug: nanoid(),
+          screen: STACK_NAMES.ExpenseForm,
+        });
+      }
     }
   };
   return (
-    <View>
+    <ScrollView>
       <View>
         <TextInput
           returnKeyLabel='done'
@@ -75,10 +146,12 @@ const SignInForm = (props: Props) => {
             margin: 0,
             padding: 0,
           }}
+          inputMode='email'
+          autoComplete='email'
           returnKeyType={'done'}
           value={userData.email}
           onChangeText={(text) =>
-            setUserData((prev) => ({ ...prev, userName: text }))
+            setUserData((prev) => ({ ...prev, email: text }))
           }
         />
         {errorState.email.isError && (
@@ -120,7 +193,7 @@ const SignInForm = (props: Props) => {
       >
         {isLoading ? '' : 'Sign In'}
       </Button>
-    </View>
+    </ScrollView>
   );
 };
 
